d49fb02dada2e3b8058ead30e895f44a9695c771
KobiOR <orr.kobi@gmail.com>
2017-05-25 17:01:07 +0300

    Update for 3 Dimension. Added right implementation for reading rgb image.

--- src/Run.java
+++ src/Run.java
@@ -14,10 +14,6 @@ public class Run {
         BufferedImage hugeImage = ImageIO.read(f);
         float[][] a=convertTo2DWithoutUsingGetRGB(hugeImage);
 
-
-        float[] ClassificationA = {1};
-        float[] ClassificationB = {0};
-
         ArrayList myTrainingInputs = new ArrayList();
         ArrayList myTrainingOutputs = new ArrayList();
 
@@ -25,13 +21,17 @@ public class Run {
             myTrainingInputs.add(arr);
             float[] temp=new float[1];
             temp[0]=new Random().nextFloat()%2;
-
             myTrainingOutputs.add(temp);
         }
 
 
         NeuralNetwork NN = new NeuralNetwork();
         NN.addLayer(10, 10);
+        NN.addLayer(10, 100);
+        NN.addLayer(100, 2);
+
+        // NN.addLayer(2, 30);
+       // NN.addLayer(2, 2);
 
         System.out.println("Begin Training");
         NN.autoTrainNetwork(myTrainingInputs, myTrainingOutputs, 0.0001f, 500000);
@@ -43,6 +43,15 @@ public class Run {
         final int height = image.getHeight();
         final boolean hasAlphaChannel = image.getAlphaRaster() != null;
 
+        for (int i = 0; i <width ; i++) {
+            for (int j = 0; j <height ; j++) {
+                int pixel=image.getRGB(j, i);
+                //int alpha = (pixel >> 24) & 0xff;
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = (pixel) & 0xff;
+            }
+        }
         float[][] result = new float[height][width];
         if (hasAlphaChannel) {
             final int pixelLength = 4;
