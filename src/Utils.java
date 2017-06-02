2269af543039599a580bd555888e79e491a5d24a
KobiOR <orr.kobi@gmail.com>
2017-06-02 16:21:57 +0300

    Added a note

--- src/Utils.java
+++ src/Utils.java
@@ -14,12 +14,12 @@ public final class Utils {
 
     public static final String GREEN_LIGHT_DIRECTORY="1";
     public static final String RED_LIGHT_DIRECTORY="2";
-    public static final String WEIGHTS_DIRECTORY = "C:\\Users\\orrko_000\\Desktop\\weights.text";
+    public static final String WEIGHTS_DIRECTORY = "weights.text";
     public static boolean LEARNING_STATE=true;
     public static int CYCLE_LIMIT=1000;
     public static float LEARNING_RATE=0.0001f;
-    static final File GREEN_DIR = new File("C:\\Users\\orrko_000\\Desktop\\A\\1");
-    static final File RED_DIR= new File("C:\\Users\\orrko_000\\Desktop\\A\\2");
+    static final File GREEN_DIR = new File("Dataset\\1");
+    static final File RED_DIR= new File("Dataset\\2");
     static final String[] EXTENSIONS = new String[]{"gif", "png", "bmp","jpeg","jpg" };
 
 
