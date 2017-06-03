8dce34eaf7741d56c038cda9e7d7253a2db73aeb
KobiOR <orr.kobi@gmail.com>
2017-06-03 20:44:11 +0300

    Server implementation added

--- src/Server.java
+++ src/Server.java
@@ -6,6 +6,8 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by orrko_000 on 03/06/2017.
@@ -31,6 +33,64 @@ public class Server {
 
         {
         }
+    }
+    private ServerSocket server;
+    private ExecutorService executor;
+       public void startServer(int maxClientsNum) {
+        executor = Executors.newFixedThreadPool(maxClientsNum);
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (true){
+                    try {
+                        System.out.print(".");
+                        Socket socket = server.accept();
+                        PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+                        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        executor.submit(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    check();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+
+                        });
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+
+    }
+    public synchronized void check() throws  Exception {
+        Thread thread = new Thread(new Runnable() {
+            public void run()
+            {
+                while(true)
+                {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+
+        });thread.start();
+
+
+
     }
 }
 
